7aa68d007615bbe2e391077265e6cd3150416494
seoeongeueun <erin9901@gmail.com>
2026-01-28 04:13:58 +0900

    style: 정렬 선택용 공용 드롭타운 컴포넌트 추가

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,3 +2,5 @@ export { Header } from "./Header";
 export { FormInput } from "./FormInput";
 export { FormRadioGroup } from "./FormRadioGroup";
 export { Button } from "./Button";
+export { Dropdown } from "./Dropdown";
+export type { DropdownOption } from "./Dropdown";
