4ac62befae5990a84016af384b9d5f7af1341e45
seoeongeueun <erin9901@gmail.com>
2026-01-28 03:36:32 +0900

    feat: 공용 인풋 컴포넌트에 required prop 추가
    - 필수 필드에 선택적으로 * 표시를 하기 위해 사용

--- components/ui/FormInput.tsx
+++ components/ui/FormInput.tsx
@@ -3,15 +3,18 @@ import { InputHTMLAttributes, forwardRef } from "react";
 interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   error?: string;
+  required?: boolean;
 }
 
 // react hook form이 참고할 수 있도록 forwardRef 사용
+// react hook form에서 required를 처리하지만 UI상 * 표시를 위해 required prop 추가
 export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
-  ({ label, error, id, ...rest }, ref) => {
+  ({ label, error, id, required = false, ...rest }, ref) => {
     return (
       <div className="flex flex-col gap-2">
         <label htmlFor={id} className="text-sm">
           {label}
+          {required && <span className="text-theme ml-1">*</span>}
         </label>
         <input
           id={id}
