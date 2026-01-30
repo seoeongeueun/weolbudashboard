0a4f079e277a487115661b63c57af66e3c3fa322
seoeongeueun <erin9901@gmail.com>
2026-01-30 11:46:28 +0900

    feat: 강의 등록 페이지 강사 권한이 없으면 접근 불가하게 변경

--- app/course/add/page.tsx
+++ app/course/add/page.tsx
@@ -1,5 +1,13 @@
 import { AddCourseForm } from "@/components/form";
+import { fetchUserProfileFromServer } from "@/lib/actions";
+import { redirect } from "next/navigation";
 
-export default function AddCoursePage() {
+export default async function AddCoursePage() {
+  //강사 권한이 없는 유저는 홈으로 리다이렉트
+  const user = await fetchUserProfileFromServer();
+  if (!user || user.role !== "INSTRUCTOR") {
+    redirect("/");
+  }
+
   return <AddCourseForm />;
 }
