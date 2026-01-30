16f4b929c75d51450e8317720b5775e86bb54228
seoeongeueun <erin9901@gmail.com>
2026-01-30 23:48:49 +0900

    fix: 로그인 폼 suspense 문제 해결

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import { LoginForm } from "@/components/form";
 import Link from "next/link";
 
 export default function Login() {
   return (
     <div className="w-full h-full flex flex-col gap-2">
-      <LoginForm />
+      <Suspense>
+        <LoginForm />
+      </Suspense>
       <Link
         href={"/auth/signup"}
         className="ml-auto text-center text-sm hover:text-theme underline underline-offset-3"
